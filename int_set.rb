d634b2238f9097156bc04db9f04f941581bde801
Timothy Callahan <tim.p.callahan@gmail.com>
2017-07-20 15:22:23 -0400

    ResizingIntSet amortizes insertion cost

--- int_set.rb
+++ int_set.rb
@@ -31,9 +31,9 @@ end
 
 class IntSet
   attr_reader :store
-  # 20 is arbitrary
-  def initialize(num_buckets = 20)
-    @store = Array.new(20) { Array.new }
+  # 10 is arbitrary
+  def initialize(num_buckets = 10)
+    @store = Array.new(num_buckets) { Array.new }
   end
 
   def insert(value)
@@ -56,5 +56,56 @@ class IntSet
 
   def num_buckets
     store.length
+  end
+end
+
+class ResizingIntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 10)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(value)
+    return false if include?(value)
+    self[value] << value
+    @count += 1
+    resize! if @count > num_buckets
+
+    value
+  end
+
+
+  def remove(value)
+    return false unless self[value].include?(value)
+    self[value].delete(value)
+    @count -= 1
+  end
+
+  def include?(value)
+    self[value].include?(value)
+  end
+
+  private
+
+  def [](value)
+    @store[value % num_buckets]
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+    old_store = @store
+    @count = 0
+    @store = Array.new(num_buckets * 2) { Array.new }
+
+    old_store.each do |bucket|
+      bucket.each do |el|
+        insert(el)
+      end
+    end
   end
 end
