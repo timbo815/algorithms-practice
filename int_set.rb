0bf62440ef2cbec903cfe67823221069c5aa5ba5
Timothy Callahan <tim.p.callahan@gmail.com>
2017-07-20 14:57:25 -0400

    IntSet stores arbitrary number of values

--- int_set.rb
+++ int_set.rb
@@ -28,3 +28,33 @@ class MaxIntSet
     raise "Out of bounds" unless (value >= 0) && (value <= max)
   end
 end
+
+class IntSet
+  attr_reader :store
+  # 20 is arbitrary
+  def initialize(num_buckets = 20)
+    @store = Array.new(20) { Array.new }
+  end
+
+  def insert(value)
+    self[value] << value unless self[value].include?(value)
+  end
+
+  def remove(value)
+    self[value].delete(value)
+  end
+
+  def include?(value)
+    self[value].include?(value)
+  end
+
+  private
+
+  def [](value)
+    store[value % num_buckets]
+  end
+
+  def num_buckets
+    store.length
+  end
+end
